5311e5c1e613f09c3e56cf3f6105b9e4e0e8ab96
Alexander Lazarević <laza@e11bits.com>
2017-08-24 11:39:16 +0200

    Use  in description

--- org2blog_install.sh
+++ org2blog_install.sh
@@ -42,7 +42,7 @@ cat <<EOF
 This script will try to do the following things:
 
  1) Checkout the latest version of org2blog, metaweblog and xml-rpc-el into $EMACSDIR
- 2) Patch xml-rpc.el using org2blog.patch so that Unicode characters work in blog posts
+ 2) Patch xml-rpc.el using $PATCHFILE so that Unicode characters work in blog posts
  3) Add credentials of your Wordpress blog to $AUTHCFG
  4) Add necessary configurations for org2blog to $EMACSCFG
 
